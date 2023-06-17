4c6e5aea5369198f6a9248acd16df9a4f2226f3b
dimysenpai <dimitrymabom@gmail.com>
2023-06-17 13:57:37 +0100

    fix side bar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,198 @@
-import React from "react";
+import React, { useState } from "react";
 import SideMenu from "../components/SideMenu";
 import TopBar from "../components/TopBar";
 
 const Home = () => {
+  const [dates, setDates] = useState({
+    "08H-10H": {
+      Lundi: {
+        salle: "C-101",
+        matiere: "mathematique",
+      },
+      Mardi: {
+        salle: "C-101",
+        matiere: "mathematique",
+      },
+      Mercredi: {
+        salle: "C-101",
+        matiere: "mathematique",
+      },
+      Jeudi: {
+        salle: "C-101",
+        matiere: "mathematique",
+      },
+      Vendredi: {
+        salle: "C-101",
+        matiere: "mathematique",
+      },
+      Samedi: {
+        salle: "C-101",
+        matiere: "mathematique",
+      },
+      Dimanche: {
+        salle: "C-101",
+        matiere: "mathematique",
+      },
+    },
+    "10H - 12H": {
+      Lundi: {
+        salle: "C-101",
+        matiere: "mathematique",
+      },
+      Mardi: {
+        salle: "C-101",
+        matiere: "mathematique",
+      },
+      Mercredi: {
+        salle: "C-101",
+        matiere: "mathematique",
+      },
+      Jeudi: {
+        salle: "C-101",
+        matiere: "mathematique",
+      },
+      Vendredi: {
+        salle: "C-101",
+        matiere: "mathematique",
+      },
+      Samedi: {
+        salle: "C-101",
+        matiere: "mathematique",
+      },
+      Dimanche: {
+        salle: "C-101",
+        matiere: "mathematique",
+      },
+    },
+    "13H - 15H": {
+      Lundi: {
+        salle: "C-101",
+        matiere: "mathematique",
+      },
+      Mardi: {
+        salle: "C-101",
+        matiere: "mathematique",
+      },
+      Mercredi: {
+        salle: "C-101",
+        matiere: "mathematique",
+      },
+      Jeudi: {
+        salle: "C-101",
+        matiere: "mathematique",
+      },
+      Vendredi: {
+        salle: "C-101",
+        matiere: "mathematique",
+      },
+      Samedi: {
+        salle: "C-101",
+        matiere: "mathematique",
+      },
+      Dimanche: {
+        salle: "C-101",
+        matiere: "mathematique",
+      },
+    },
+    "15H - 17H": {
+      Lundi: {
+        salle: "C-101",
+        matiere: "mathematique",
+      },
+      Mardi: {
+        salle: "C-101",
+        matiere: "mathematique",
+      },
+      Mercredi: {
+        salle: "C-101",
+        matiere: "mathematique",
+      },
+      Jeudi: {
+        salle: "C-101",
+        matiere: "mathematique",
+      },
+      Vendredi: {
+        salle: "C-101",
+        matiere: "mathematique",
+      },
+      Samedi: {
+        salle: "C-101",
+        matiere: "mathematique",
+      },
+      Dimanche: {
+        salle: "C-101",
+        matiere: "mathematique",
+      },
+    },
+    "17H - 19H": {
+      Lundi: {
+        salle: "C-101",
+        matiere: "mathematique",
+      },
+      Mardi: {
+        salle: "C-101",
+        matiere: "mathematique",
+      },
+      Mercredi: {
+        salle: "C-101",
+        matiere: "mathematique",
+      },
+      Jeudi: {
+        salle: "C-101",
+        matiere: "mathematique",
+      },
+      Vendredi: {
+        salle: "C-101",
+        matiere: "mathematique",
+      },
+      Samedi: {
+        salle: "C-101",
+        matiere: "mathematique",
+      },
+      Dimanche: {
+        salle: "C-101",
+        matiere: "mathematique",
+      },
+    },
+    "19H - 21H": {
+      Lundi: {
+        salle: "C-101",
+        matiere: "mathematique",
+      },
+      Mardi: {
+        salle: "C-101",
+        matiere: "mathematique",
+      },
+      Mercredi: {
+        salle: "C-101",
+        matiere: "mathematique",
+      },
+      Jeudi: {
+        salle: "C-101",
+        matiere: "mathematique",
+      },
+      Vendredi: {
+        salle: "C-101",
+        matiere: "mathematique",
+      },
+      Samedi: {
+        salle: "C-101",
+        matiere: "mathematique",
+      },
+      Dimanche: {
+        salle: "C-101",
+        matiere: "mathematique",
+      },
+    },
+  });
+
   return (
     <div className="home_page">
       <SideMenu />
       <div className="center_homepage">
         <TopBar />
-        <div className="intiule_filiere">FILIERE : GENIE LOGICIEL</div>
+        <div></div>
+        {/* <div className="intiule_filiere">FILIERE : GENIE LOGICIEL</div> */}
         <div className="intiule_week">
           Semaine:{" "}
           <span style={{ color: "var(--danger)" }}>Du 10 au 17 mai 2021</span>
@@ -23,131 +207,25 @@ const Home = () => {
             <div className="day">Vendredi</div>
             <div className="day">Samedi</div>
             <div className="day">Dimanche</div>
+            {/* BEGIN */}
+            {Object.keys(dates).map((time) => (
+              <>
+                <div className="hour">{time}</div>
+                {Object.keys(dates[time]).map((day) => (
+                  <div className="matiere">
+                    <span>{dates[time][day].matiere}</span>
+                    <span className="salle">{dates[time][day].salle}</span>
+                  </div>
+                ))}
+              </>
+            ))}
+            {/* END */}
           </div>
-          <div className="row">
-            <div className="hour">08H - 09H</div>
-            <div className="matiere">
-              <span>Lundi</span>
-              <span className="salle">C - 201</span>
-            </div>
-            <div className="matiere">
-              <span>Mardi</span>
-              <span className="salle">C - 201</span>
-            </div>
-            <div className="matiere">
-              <span>Mercredi</span>
-              <span className="salle">C - 201</span>
-            </div>
-            <div className="matiere">
-              <span>Jeudi</span>
-              <span className="salle">C - 201</span>
-            </div>
-            <div className="matiere">
-              <span>Vendredi</span>
-              <span className="salle">C - 201</span>
-            </div>
-            <div className="matiere">
-              <span>Samedi</span>
-              <span className="salle">C - 201</span>
-            </div>
-            <div className="matiere">
-              <span>Dimanche</span>
-              <span className="salle">C - 201</span>
-            </div>
-
-            <div className="hour">08H - 09H</div>
-            <div className="matiere">
-              <span>Lundi</span>
-              <span className="salle">C - 201</span>
-            </div>
-            <div className="matiere">
-              <span>Mardi</span>
-              <span className="salle">C - 201</span>
-            </div>
-            <div className="matiere">
-              <span>Mercredi</span>
-              <span className="salle">C - 201</span>
-            </div>
-            <div className="matiere">
-              <span>Jeudi</span>
-              <span className="salle">C - 201</span>
-            </div>
-            <div className="matiere">
-              <span>Vendredi</span>
-              <span className="salle">C - 201</span>
-            </div>
-            <div className="matiere">
-              <span>Samedi</span>
-              <span className="salle">C - 201</span>
-            </div>
-            <div className="matiere">
-              <span>Dimanche</span>
-              <span className="salle">C - 201</span>
-            </div>
-
-            <div className="hour">08H - 09H</div>
-            <div className="matiere" style={{ gridRow: "span 2" }}>
-              <span>Lundi</span>
-              <span className="salle">C - 201</span>
-            </div>
-            <div className="matiere">
-              <span>Mardi</span>
-              <span className="salle">C - 201</span>
-            </div>
-            <div className="matiere">
-              <span>Mercredi</span>
-              <span className="salle">C - 201</span>
-            </div>
-            <div className="matiere">
-              <span>Jeudi</span>
-              <span className="salle">C - 201</span>
-            </div>
-            <div className="matiere">
-              <span>Vendredi</span>
-              <span className="salle">C - 201</span>
-            </div>
-            <div className="matiere">
-              <span>Samedi</span>
-              <span className="salle">C - 201</span>
-            </div>
-            <div className="matiere">
-              <span>Dimanche</span>
-              <span className="salle">C - 201</span>
-            </div>
-
-            <div className="hour">08H - 09H</div>
-
-            <div className="matiere">
-              <span>Mardi</span>
-              <span className="salle">C - 201</span>
-            </div>
-            <div className="matiere">
-              <span>Mercredi</span>
-              <span className="salle">C - 201</span>
-            </div>
-            <div className="matiere">
-              <span>Jeudi</span>
-              <span className="salle">C - 201</span>
-            </div>
-            <div className="matiere">
-              <span>Vendredi</span>
-              <span className="salle">C - 201</span>
-            </div>
-            <div className="matiere">
-              <span>Samedi</span>
-              <span className="salle">C - 201</span>
-            </div>
-            <div className="matiere">
-              <span>Dimanche</span>
-              <span className="salle">C - 201</span>
-            </div>
+          <div className="footer_nb">
+            NB: Les étudiants sont invités à consulter régulièrement le tableau
+            d'affichage car des modifications pourront etre apportées aux
+            emplois de temps.
           </div>
-        </div>
-
-        <div className="footer_nb">
-          NB: Les étudiants sont invités à consulter régulièrement le tableau
-          d'affichage car des modifications pourront etre apportées aux emplois
-          de temps.
         </div>
       </div>
     </div>
